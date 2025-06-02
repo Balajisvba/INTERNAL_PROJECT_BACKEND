714ffff034044a3239a401651acdb28adf090da9
balaji <balajisvba@gmail.com>
2025-06-02 15:01:58 +0530

    project Created

---
+++ src/main/java/com/tvm/internal/tvm_internal_project/TvmInternalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tvm.internal.tvm_internal_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmInternalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmInternalProjectApplication.class, args);
+	}
+
+}
