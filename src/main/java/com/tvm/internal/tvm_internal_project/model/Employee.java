6a39d57ca6ac4418a322f6dcb7eec1ce47549b6a
JeyaPradeesh <jeyapradeesh762@gmail.com>
2025-06-03 10:26:16 +0530

    TVM Backend Initial commit

---
+++ src/main/java/com/tvm/internal/tvm_internal_project/model/Employee.java
@@ -0,0 +1,48 @@
+package com.tvm.internal.tvm_internal_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer employeeId; // Required
+    private String employeeName; // Required
+    private String role; // Optional
+    private String status; // Optional
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
